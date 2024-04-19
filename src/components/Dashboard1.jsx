e8373352dd3e5ed46a47362f602998492ca0c80a
Hernan A. Pinetta <pinettah@gmail.com>
2024-04-19 12:54:06 -0300

    Soluciones graficas 2

--- src/components/Dashboard1.jsx
+++ src/components/Dashboard1.jsx
@@ -4,9 +4,6 @@ import Inicio from './Inicio'
 const Dashboard1 = () => {
     return (
         <>
-        
-        <div className='flex p-4 bg-slate-400 w-screen justify-center rounded-tl-lg'></div>
-
         <div className='flex p-4 bg-slate-400 w-screen justify-center rounded-lg'>
 
             <Inicio/>
