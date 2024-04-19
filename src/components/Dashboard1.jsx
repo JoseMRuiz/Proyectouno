bed8a250d570f9348500a426ca920e35a2e58536
Hernan A. Pinetta <pinettah@gmail.com>
2024-04-19 10:31:55 -0300

    Arreglado vista inicio de sesion

--- src/components/Dashboard1.jsx
+++ src/components/Dashboard1.jsx
@@ -3,14 +3,10 @@ import Inicio from './Inicio'
 
 const Dashboard1 = () => {
     return (
-<<<<<<< HEAD
         <div className='flex p-4 bg-slate-400 w-screen justify-center rounded-tl-lg'>
-=======
-        <div className='flex p-4 bg-slate-400 w-screen justify-center rounded-lg'>
->>>>>>> 75b937cfd23335bfe6f6b8f5795014b17af12fbf
-            <Inicio/>
+            <Inicio />
         </div>
-    
+
     )
 }
 
